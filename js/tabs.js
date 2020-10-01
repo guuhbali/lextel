58e486bf71e8e9e87b94911efb0289ad4495426d
guuhbali <46822443+guuhbali@users.noreply.github.com>
2020-10-01 14:05:42 -0300

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,13 @@
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  }
+  tablinks = document.getElementsByClassName("tabs_b");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
